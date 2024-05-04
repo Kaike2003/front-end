b3cadf500c5793a27660c2d300c475f5cacd5be1
Kaike Bartolomeu <102388705+Kaike2003@users.noreply.github.com>
2024-05-04 04:56:34 +0100

    final

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Router from './router';
+import { Toaster } from 'react-hot-toast';
+import { AuthProvider } from './context/AutenticacaoContext';
+import ModalAchado from './components/layout/Modals/ModalAchado/ModalAchado';
+import ModalPerdido from './components/layout/Modals/ModalPerdido/ModalPerdido';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AuthProvider>
+      <React.Fragment>
+        <div className='bg-gray-100 h-screen'>
+          <Toaster
+            position="top-center"
+            reverseOrder={false}
+          />
+          <ModalAchado />
+          <ModalPerdido />
+          <Router />
+        </div>
+      </React.Fragment>
+    </AuthProvider>
   );
 }
 
